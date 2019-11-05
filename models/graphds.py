e8fe554cd656bf1852275a6167153b39b3490ad7
Raynold Ng <raynold.ng24@gmail.com>
2019-11-05 15:14:31 +0800

    add deep sets

---
+++ models/graphds.py
@@ -0,0 +1,199 @@
+"""Deep Sets
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import sys
+
+sys.path.append("models/")
+from mlp import MLP
+
+class GraphDS(nn.Module):
+    # TODO add back in learn_eps
+    def __init__(
+        self,
+        num_layers,
+        num_mlp_layers,
+        input_dim,
+        hidden_dim,
+        output_dim,
+        final_dropout,
+        graph_pooling_type,
+        neighbor_pooling_type,
+        device,
+    ):
+        """
+            num_layers: number of layers in the neural networks (INCLUDING the input layer)
+            num_mlp_layers: number of layers in mlps readout layer (EXCLUDING the input layer)
+            input_dim: dimensionality of input features
+            output_dim: number of classes for prediction
+            final_dropout: dropout ratio on the final linear layer
+            neighbor_pooling_type: how to aggregate neighbors (mean, average, or max)
+            graph_pooling_type: how to aggregate entire nodes in a graph (mean, average)
+            device: which device to use
+        """
+
+        super(GraphDS, self).__init__()
+
+        self.final_dropout = final_dropout
+        self.device = device
+        self.num_layers = num_layers
+        self.graph_pooling_type = graph_pooling_type
+        self.neighbor_pooling_type = neighbor_pooling_type
+
+        # TODO add back support for this
+        self.learn_eps = False
+        self.batch_norms = torch.nn.ModuleList()
+
+        self.phi = MLP(num_mlp_layers, input_dim * num_layers, hidden_dim, hidden_dim)        
+        self.rho = MLP(num_mlp_layers, hidden_dim, hidden_dim, output_dim)        
+
+        # Linear function that maps the hidden representation at dofferemt layers into a prediction score
+        # self.linears_prediction = torch.nn.ModuleList()   
+        for layer in range(num_layers):
+            self.batch_norms.append(nn.BatchNorm1d(input_dim))
+        
+    def __preprocess_neighbors_maxpool(self, batch_graph):
+        ###create padded_neighbor_list in concatenated graph
+
+        # compute the maximum number of neighbors within the graphs in the current minibatch
+        max_deg = max([graph.max_neighbor for graph in batch_graph])
+
+        padded_neighbor_list = []
+        start_idx = [0]
+
+        for i, graph in enumerate(batch_graph):
+            start_idx.append(start_idx[i] + len(graph.g))
+            padded_neighbors = []
+            for j in range(len(graph.neighbors)):
+                # add off-set values to the neighbor indices
+                pad = [n + start_idx[i] for n in graph.neighbors[j]]
+                # padding, dummy data is assumed to be stored in -1
+                pad.extend([-1] * (max_deg - len(pad)))
+
+                # Add center nodes in the maxpooling if learn_eps is False, i.e., aggregate center nodes and neighbor nodes altogether.
+                if not self.learn_eps:
+                    pad.append(j + start_idx[i])
+
+                padded_neighbors.append(pad)
+            padded_neighbor_list.extend(padded_neighbors)
+
+        return torch.LongTensor(padded_neighbor_list)
+
+    def __preprocess_neighbors_sumavepool(self, batch_graph):
+        ###create block diagonal sparse matrix
+
+        edge_mat_list = []
+        start_idx = [0]
+        for i, graph in enumerate(batch_graph):
+            start_idx.append(start_idx[i] + len(graph.g))
+            edge_mat_list.append(graph.edge_mat + start_idx[i])
+        Adj_block_idx = torch.cat(edge_mat_list, 1)
+        Adj_block_elem = torch.ones(Adj_block_idx.shape[1])
+
+        # Add self-loops in the adjacency matrix if learn_eps is False, i.e., aggregate center nodes and neighbor nodes altogether.
+
+        if not self.learn_eps:
+            num_node = start_idx[-1]
+            self_loop_edge = torch.LongTensor([range(num_node), range(num_node)])
+            elem = torch.ones(num_node)
+            Adj_block_idx = torch.cat([Adj_block_idx, self_loop_edge], 1)
+            Adj_block_elem = torch.cat([Adj_block_elem, elem], 0)
+
+        Adj_block = torch.sparse.FloatTensor(
+            Adj_block_idx, Adj_block_elem, torch.Size([start_idx[-1], start_idx[-1]])
+        )
+
+        return Adj_block.to(self.device)
+
+    def __preprocess_graphpool(self, batch_graph):
+        ###create sum or average pooling sparse matrix over entire nodes in each graph (num graphs x num nodes)
+
+        start_idx = [0]  # ray: prefix sum of graph nodes
+
+        # compute the padded neighbor list
+        for i, graph in enumerate(batch_graph):
+            start_idx.append(start_idx[i] + len(graph.g))
+
+        idx = []
+        elem = []
+        for i, graph in enumerate(batch_graph):
+            ###average pooling
+            if self.graph_pooling_type == "average":
+                elem.extend([1.0 / len(graph.g)] * len(graph.g))
+
+            else:
+                ###sum pooling
+                elem.extend([1] * len(graph.g))
+
+            idx.extend([[i, j] for j in range(start_idx[i], start_idx[i + 1], 1)])
+        elem = torch.FloatTensor(elem)
+        idx = torch.LongTensor(idx).transpose(0, 1)
+        graph_pool = torch.sparse.FloatTensor(
+            idx, elem, torch.Size([len(batch_graph), start_idx[-1]])
+        )
+
+        return graph_pool.to(self.device)
+
+    def maxpool(self, h, padded_neighbor_list):
+        ###Element-wise minimum will never affect max-pooling
+
+        dummy = torch.min(h, dim=0)[0]
+        h_with_dummy = torch.cat([h, dummy.reshape((1, -1)).to(self.device)])
+        pooled_rep = torch.max(h_with_dummy[padded_neighbor_list], dim=1)[0]
+        return pooled_rep
+
+    def next_layer(self, h, layer, padded_neighbor_list = None, Adj_block = None):
+        """
+        h: feature tensor
+        layer (int): layer number
+
+        NOTE removing all instances of non linearity
+        """
+        ###pooling neighboring nodes and center nodes altogether  
+            
+        if self.neighbor_pooling_type == "max":
+            ##If max pooling
+            pooled = self.maxpool(h, padded_neighbor_list)
+        else:
+            #If sum or average pooling
+            pooled = torch.spmm(Adj_block, h)
+            if self.neighbor_pooling_type == "average":
+                #If average pooling
+                degree = torch.spmm(Adj_block, torch.ones((Adj_block.shape[0], 1)).to(self.device))
+                pooled = pooled/degree
+
+        return pooled
+
+    def forward(self, batch_graph):
+        X_concat = torch.cat([graph.node_features for graph in batch_graph], 0).to(self.device)
+        graph_pool = self.__preprocess_graphpool(batch_graph)
+
+        if self.neighbor_pooling_type == "max":
+            padded_neighbor_list = self.__preprocess_neighbors_maxpool(batch_graph)
+        else:
+            Adj_block = self.__preprocess_neighbors_sumavepool(batch_graph)
+
+        #list of hidden representation at each layer (including input)
+        hidden_rep = [X_concat]
+        h = X_concat
+
+        for layer in range(self.num_layers-1):
+            # no learn_eps for now
+            if self.neighbor_pooling_type == "max":
+                h = self.next_layer(h, layer, padded_neighbor_list = padded_neighbor_list)
+            elif not self.neighbor_pooling_type == "max":
+                h = self.next_layer(h, layer, Adj_block = Adj_block)
+
+            hidden_rep.append(h)
+
+        # do not use batch pooling first
+        hidden_rep = [self.batch_norms[layer](h) for layer, h in enumerate(hidden_rep)]
+        hidden_rep = torch.cat(hidden_rep, dim=1) # concat to get wider 
+
+        rep = self.phi(hidden_rep)
+        # import pdb; pdb.set_trace()
+        result = self.rho(torch.spmm(graph_pool, rep))
+
+        return result
